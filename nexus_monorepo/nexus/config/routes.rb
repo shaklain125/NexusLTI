f9891c2fc66845142da3468c471d2cbe0b6d25f8
shaklain <48572696+shaklain125@users.noreply.github.com>
2021-08-24 16:33:07 +0100

    added lti_paths from init class
    Signed-off-by: shaklain <48572696+shaklain125@users.noreply.github.com>

--- nexus_monorepo/nexus/config/routes.rb
+++ nexus_monorepo/nexus/config/routes.rb
@@ -100,7 +100,7 @@ Rails.application.routes.draw do
 
   ### LTI
   scope(controller: :lti) do
-    LtiUtils::RHHelper.init_lti_paths!(self)
+    LtiUtils::Init.lti_paths!(self)
     get 'lti/configure', action: :configure, as: :lti_configure
     post 'lti/configure/generate', action: :configure_generate, as: :lti_configure_generate
     get 'lti/manage_assignment', action: :manage_assignment, as: :lti_manage_assignment
